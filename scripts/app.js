c1e52b9e8db870f8253ce0d77da68e0418686e1d
mjavedjs <mjavedjs2006@gmail.com>
2025-03-24 23:58:40 -0700

    renderProfileAndName

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,49 @@
-const trybtn = document.querySelector("#trybtn").addEventListener("click",()=>{
-     window.location = 'singup.html'
-})
+import { getDocs ,  where , query, collection} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+import {auth,db} from "./config.js";
+import {onAuthStateChanged} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
 
+   const userProfile = document.querySelector(".profile-img");
+   const userName = document.querySelector('#name');
+   const trybtn = document.querySelector("#trybtn").addEventListener('click',()=>{
+       window.location = 'singup.html'
+   })
 
-// 
+   onAuthStateChanged(auth, async(user) => {
+     if (user) {
+       // User is signed in, see docs for a list of available properties
+       // https://firebase.google.com/docs/reference/js/auth.user
+       const uid = user.uid;
+       console.log(uid)
+         getdata(uid);
+ 
+       // ...
+     } else {
+       // User is signed out
+       console.log('singout ')
+       
+       // ...
+     }
+   });
+ async function getdata(uid){
+     let user = null
+     try {
+          const q = query(collection(db, "users"),where("uid", "==",uid)  )
+          const docSnap = await getDocs(q);   
+    if (docSnap) {
+      docSnap.forEach((doc) => {
+          console.log("User Data:", doc.data());
+          userName.textContent = doc.data().name;
+          userProfile.src = doc.data().profileImg || "default-profile.png"; // Fallback image
+        });
+} 
+else {
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
+}
+     } catch (error) {
+          console.log('Fetcthing data error',error)
+     }
+     return user
+     
+}
+
